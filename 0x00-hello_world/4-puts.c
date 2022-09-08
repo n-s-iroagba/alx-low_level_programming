e1f1bbd2eba566c22b39870f42749bd9e29be1d7
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-09-08 12:44:12 +0100

    update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
 *Return: Always 0 (success)X
 */
 
-int main()
+int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
