3282b22610ed1716c48a22bc5cf32fa84481b553
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-09-08 21:21:27 +0100

    update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
 */
 int main(void)
 {
-char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-fprintf(stderr, "%s", str);
+fprintf(stderr, "%s",
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 
 }
