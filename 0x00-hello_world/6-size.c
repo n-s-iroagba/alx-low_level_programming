6721515a6d9423b8a3b334b1dd863e1b6dbe69bc
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-09-08 14:01:17 +0100

    create 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ long int d;
 float e;
 printf("Size of a char: %u byte(s)\n", (unsigned int)sizeof(a));
 printf("Size of an int: %u byte(s)\n", (unsigned int)sizeof(b));
-printf("Size of a long: %u byte(s)\n", (unsigned int)sizeof(c));
+printf("Size of a long int: %u byte(s)\n", (unsigned int)sizeof(c));
 printf("Size of a long long int: %u byte(s)\n", (unsigned int)sizeof(d));
 printf("Size of a float: %u byte(s)\n", (unsigned int)sizeof(e));
 return (0);
