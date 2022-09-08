39de88adc33b3581d1f4c72f30f9e1673673e6d5
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-09-08 20:53:54 +0100

    create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return:1 not successful
+*/
+int main(void)
+{
+if (5 > 6)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
+{
+return (0);
+}
+}
