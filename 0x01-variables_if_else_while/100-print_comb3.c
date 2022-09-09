9bed1c3bcacb00ef492b319f0c5d946986ba8050
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-09-09 21:23:20 +0100

    update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,8 +10,8 @@ for (i = 0; i <= 9; i++)
 {
 int j = i + 1;
 for (; j <= 9; j++)
-putchar(i % n + '0');
-putchar(j % n + '0');
+putchar(i % 10 + '0');
+putchar(j % 10 + '0');
 putchar(",");
 putchar(" ");
 }
