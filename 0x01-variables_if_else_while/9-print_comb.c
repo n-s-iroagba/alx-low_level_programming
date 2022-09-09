9515b07c71bf9146cff5397e5d6a982429e2d5dc
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-09-09 17:16:44 +0100

    create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: 0 (successfull)
+*/
+int main(void)
+{
+int i = 0;
+for (i = 0; i <= 9; i++)
+{
+putchar(i % 10 + '0')
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
+
