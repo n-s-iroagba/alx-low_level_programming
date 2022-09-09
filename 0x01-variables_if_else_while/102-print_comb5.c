6688b61178d8096a04d0699173601413f6b23455
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-09-09 22:49:19 +0100

    create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: 0 (successfull)
+*/
+int main(void)
+{
+int d1, d2, d3, d4, n = 0;
+
+for (d1 = '0'; d1 <= '9'; d1++)
+{
+for (d2 = '0'; d2 <= '9'; d2++)
+{
+for (d3 = '0'; d3 <= '9'; d2++)
+{
+for (d4 = d3 + 1; d3 <= '9'; d3++, n++)
+{
+if (n > 0)
+{
+putchar(',');
+putchar(' ');
+}
+putchar(d1);
+putchar(d2);
+putchar(d3);
+putchar(d4);
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
+
+
