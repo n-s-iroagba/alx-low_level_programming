ea49cb4019dc33f2f8f0d286441a31299d7aced9
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-09-09 16:42:21 +0100

    create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: 0 (successfull)
+*/
+int main(void)
+{
+int i = 0;
+for (i = 0; i <= 9; i++)
+
+putchar(i % 10 + '0');
+
+putchar('\n');
+return (0);
+}
