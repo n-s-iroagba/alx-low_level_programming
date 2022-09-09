a5ee4929c8c4198730793261ae7b78a75728920a
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-09-09 15:48:30 +0100

    create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: 0 (successfull)
+*/
+int main(void)
+{
+char ch = 'a';
+char cb = 'A'
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (cb = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
