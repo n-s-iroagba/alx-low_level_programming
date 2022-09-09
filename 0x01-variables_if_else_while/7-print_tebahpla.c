f09cbfe78570792c4d3d324ad8fbfd4e0877c4eb
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-09-09 16:48:45 +0100

    create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: 0 (successfull)
+*/
+int main(void)
+{
+char ch = 'z';
+
+for (ch = 'z'; ch => 'a'; ch--)
+putchar(ch);
+putchar('\n');
+return (0);
+}
+
