e355b5ff465f0cdc02e506b5f39e69aa7e748ab8
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-09-09 16:15:21 +0100

    create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: 0 (successfull)
+*/
+int main(void)
+{
+int i = 0;
+for (i = 0; i <= 9; i++)
+{
+putchar(i);
+}
+putchar("\n");
+return (0);
+}
