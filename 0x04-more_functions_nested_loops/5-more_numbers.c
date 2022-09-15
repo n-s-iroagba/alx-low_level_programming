2306478cc3276751c277b30001dfb0f606320697
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-09-15 16:01:25 +0100

    fix bug in 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,3 +1,4 @@
+
 #include"main.h"
 
 /**
@@ -16,6 +17,11 @@ void more_numbers(void)
 		for (count = 0; count <= 14; ++count)
 		{
 			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
 			_putchar(num + 48);
 		}
 		_putchar('\n');
