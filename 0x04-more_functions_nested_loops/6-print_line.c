e2c75137bc2b269cbab85a79a8623dffb74570f1
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-09-15 16:04:54 +0100

    fix bug 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,11 +12,11 @@ void print_line(int n)
 	int lnChr;
 
 	if (n <= 0)
-		_putchar('\n');
+		putchar('\n');
 	else
 	{
 		for (lnChr = 1; lnChr <= n; ++lnChr)
 			_putchar('_');
-		_putchar('\n');
+		putchar('\n');
 	}
 }
