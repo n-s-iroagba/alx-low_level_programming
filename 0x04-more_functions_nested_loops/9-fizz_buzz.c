458eccc27ca29db747021296c3ab06dae6f63505
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-09-15 17:06:06 +0100

    fix bug

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,25 +6,25 @@
 
 int main(void)
 {
-int num;
-for (num = 1; num <= 100; num++)
-{
-if (num % 3 == 0)
-{
-printf("Fizz");
-}
-else if  (num % 5 == 0)
-{
-printf("Buzz");
-}
-else
-{
-printf("%i", num);
-}
-putchar('\n');
-}
+int n;
 
+for (n = 1; n <= 100; n++)
+	{
+	if ((n % 3 == 0) && (n % 5 == 0))
+		printf(" FizzBuzz");
 
+	else if (n % 5 == 0)
+		printf(" Buzz");
 
+	else if (n % 3 == 0)
+		printf(" Fizz");
 
+	else if (n == 1)
+		printf("%i", n);
+	else
+		printf(" %i", n);
+	}
+
+printf("\n");
+return (0);
 }
