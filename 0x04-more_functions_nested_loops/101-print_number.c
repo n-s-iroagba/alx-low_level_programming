c958e5c41a6b5ca7000f05af3431251cd4bd80ad
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-09-16 01:28:48 +0100

    fix logic

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,5 @@
 #include"main.h"
-
+#include <stdio.h>
 /**
  * print_number - Prints an integer.
  *
@@ -8,11 +8,23 @@
 
 void print_number(int n)
 {
-unsigned int num = n;
-putchar(num + '0');
-putchar('\n');
+	// If number is smaller than 0, put a - sign
+	// and change number to positive
+	if (n < 0) {
+		putchar('-');
+		n = -n;
+	}
 
+	// Remove the last digit and recur
+	if (n/10)
+		print_number(n/10);
 
+	// Print the last digit
+	putchar(n%10 + '0');
 }
+
+
+
+
 
 
